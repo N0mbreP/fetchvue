3743c67f0b9bd8528970a6b1532b539a28d932f4
N0mbreP <pol.paredes.7e7@itb.cat>
2025-12-11 16:56:41 +0100

    Falta css y canviar de lloc

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {ref} from "vue";
 import {useFetch} from "@/composables/useFetch";
-const cerca=ref('chicken');
+const cerca=ref('Breakfast');
 const url= ref(`https://www.themealdb.com/api/json/v1/1/search.php?s=${cerca.value}`);
 const {data, error, loading, fetchData}= useFetch(url);
 console.log(data.value);
@@ -11,6 +11,14 @@ console.log(data.value);
   <div v-if="loading">Loading...</div>
   <div v-else-if="error">Error: {{ error }}</div>
   <div v-else>
-    <div v-if="data && data.meals">{{data.meals}}</div>
+    <!--<div v-if="data && data.meals">{{data.meals}}</div>-->
+    <div v-if="data && data.meals">
+      <div v-for="meal in data.meals" :key="meal.idMeal">
+        <h2>{{ meal.strMeal }}</h2>
+        <img :src="meal.strMealThumb" :alt="meal.strMeal" width="200" />
+        <p>{{ meal.strCategory }} - {{ meal.strArea }}</p>
+        <p>{{ meal.strInstructions }}</p> 
+        </div>
+        </div>
   </div>
 </template>
